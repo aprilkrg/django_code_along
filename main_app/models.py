e44f38b715e89af75d44ce38e5cfc2577eb7d47c
aprilkrgonzales <april.gonzales@generalassemb.ly>
2022-04-10 21:32:06 -0600

    complete plan.md

---
+++ main_app/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Show(models.Model):
+    title = models.CharField(max_length=100)
+    genre = models.CharField(max_length=50)
+    premiere_date = models.DateField(auto_now=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='shows', default=None)
+    class Rating(models.IntegerChoices):
+        LIKE_IT = 1
+        LOVE_IT = 2
+        GOTTA_HAVE_IT = 3
+    review = models.IntegerField(choices=Rating.choices)
+
+    def __str__(self):
+        return self.title
---
+++ main_app/views.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+from .models import Show
+from .form import ShowForm
+
+def home(request):
+    print('HOME PAGE')
+    return HttpResponse('<h1> (╯°□°）╯︵ ┻━┻ </h1>')
+
+def shows(request):
+    shows = Show.objects.all()
+    return render(request, 'shows_list.html', {'shows': shows})
+
+@login_required(login_url='/login/')
+def show_create(request):
+    if request.method == 'POST':
+        form = ShowForm(request.POST)
+        form.instance.user = request.user
+        if form.is_valid():
+            show = form.save()
+            return redirect('profile')
+    else:
+        form = ShowForm()
+    context = {'form': form, 'header': 'Add new tv show', 'user': request.user}
+    return render(request, 'show_form.html', context)
+
+@login_required(login_url='/login/')
+def show_edit(request, pk):
+    show = Show.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = ShowForm(request.POST, instance=show)
+        if form.is_valid():
+            show = form.save()
+            return redirect('shows')
+    else: 
+        form = ShowForm(instance=show)
+    return render(request, 'show_form.html', {'form': form})
+
+@login_required(login_url='/login/')
+def show_delete(request, pk):
+    Show.objects.get(pk=pk).delete()
+    return redirect('shows')
+
+def login_page(request):
+    return render(request, 'login_form.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('shows')
+
+def profile_show(request):
+    if request.user.is_authenticated:
+        shows = Show.objects.filter(user_id=request.user.id)
+        return render(request, 'profile.html', {'shows': shows})
+    else:
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request,user)
+            return render(request, 'profile.html')
+        else:
+            print("SOMETHING WENT WRONG WITH LOGIN")
+            return redirect('login_page')
